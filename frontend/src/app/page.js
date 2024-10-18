0d946d3771d2f20944b49ff12042c32f03d70d27
CyrilDemand <cydemand@gmail.com>
2024-10-18 14:32:45 +0200

    header

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Header from "@/app/ui/Header";
+import background from "@/app/public/img/background_header.jpg"
+import BackgroundHeader from "@/app/ui/BackgroundHeader";
 
 export default function Home() {
   return (
-    <div>
-      Hello world !
-    </div>
+      <div>
+          <Header/>
+          <BackgroundHeader/>
+      </div>
   );
 }
---
+++ frontend/src/app/ui/BackgroundHeader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import background from "@/app/public/img/background_header.jpg";
+import Image from "next/image";
+
+const BackgroundHeader = () => {
+    return (
+        <div>
+            {/* Image d'arrière-plan floue */}
+            <Image
+                src={background}
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                priority={true}
+                className="blur-sm"
+            />
+
+            {/* Texte centré au milieu de l'image */}
+            <div className="absolute inset-0 flex flex-col justify-center items-center text-white z-10">
+                <h1 className="text-5xl font-bold mb-4">Les ventes à -50%</h1>
+                <button className={"text-2xl bg-amber-400 text-white px-5 py-6"}>voir nos peluches</button>
+            </div>
+        </div>
+    );
+};
+
+export default BackgroundHeader;
